4d8e4db66c8bf82c0da7300fd62aaefa67a9a36c
Narayan Iyengar <narayan-pan@users.noreply.github.com>
2016-10-13 09:52:33 -0700

    update iptables to make ip fwd permanent

--- standalone-nat/nat-iptables.sh
+++ standalone-nat/nat-iptables.sh
@@ -8,7 +8,9 @@ sudo iptables -F
 sudo iptables -X
  
 # enable IP forwarding
-echo 1 > /proc/sys/net/ipv4/ip_forward
+#echo 1 > /proc/sys/net/ipv4/ip_forward
+# Make IP forwarding permanent
+sudo sysctl -w net.ipv4.ip_forward=1
  
 # For login to NAT machine using ssh -p 50022
 sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 50022 -j DNAT --to $1.5.4:22
